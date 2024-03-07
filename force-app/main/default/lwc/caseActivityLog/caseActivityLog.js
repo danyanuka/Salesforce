b9f94f32390e60471ce1096953293aaea729fb8f
danyanuka <danyanuka@gmail.com>
2024-03-07 18:43:39 +0200

    managed to get activity from a case

--- force-app/main/default/lwc/caseActivityLog/caseActivityLog.js
+++ force-app/main/default/lwc/caseActivityLog/caseActivityLog.js
@@ -1,56 +1,45 @@
 import { LightningElement, wire, api, track } from "lwc";
-import { getRecord, recordUpdated } from "lightning/uiRecordApi";
+// import { getRecord, recordUpdated } from "lightning/uiRecordApi";
+import getCaseActivityLog from "@salesforce/apex/CaseHistoryController.getCaseActivityLog";
 // import CASE_OBJ from "@salesforce/schema/Case"; // Represents the Case object
 
-const FIELDS = [
-  "Case.CaseNumber",
-  "Case.SuppliedEmail",
-  "Case.ContactId",
-  "Case.OwnerId",
-  "Case.Priority",
-  "Case.Subject",
-  "Case.Description"
-];
+// const FIELDS = [
+//   "Case.CaseNumber",
+//   "Case.SuppliedEmail",
+//   "Case.ContactId",
+//   "Case.Status",
+//   "Case.OwnerId",
+//   "Case.Priority"
+// ];
 
 export default class CaseActivityLog extends LightningElement {
   @api recordId; // Holds the current record Id (@api for var to be public)
   // @track caseData;
-  @track activityLog = [];
+  @track activityLog;
 
-  @wire(getRecord, {
-    recordId: "$recordId",
-    fields: FIELDS
-  })
-  wiredRecordData({ data, error }) {
+  @wire(getCaseActivityLog, { caseId: "$recordId" })
+  wiredActivityLog({ data, error }) {
     if (data) {
-      // this.caseData = data.fields;
-      console.log("data : ", data);
+      this.activityLog = data;
+      console.log(this.activityLog);
     } else if (error) {
-      console.error("Error:", error);
+      console.log("Error:", error);
     }
   }
 
+  // @wire(getRecord, {
+  //   recordId: "$recordId",
+  //   fields: FIELDS
+  // })
+  // caseObjectData({ data, error }) {
+  //   if (data) {
+  //     this.caseData = data.fields;
+  //     console.log("data : ", data.fields);
+  //   } else if (error) {
+  //     console.error("Error:", error);
+  //   }
+  // }
   connectedCallback() {
-    // Register record updated event listener on component initialization, filtering for **Case object**
-    recordUpdated.addEventListener("case", this.handleRecordUpdated);
-  }
-
-  disconnectedCallback() {
-    // Remove event listener on component cleanup
-    recordUpdated.removeEventListener("case", this.handleRecordUpdated);
-  }
-  handleRecordUpdated(event) {
-    console.log(event);
-    // const changedFields = event.detail.changedFields;
-    // const newLogEntry = {
-    //   timestamp: new Date(),
-    //   changedBy: changedFields.OwnerId.displayValue,
-    //   changes: Object.keys(changedFields).map((field) => ({
-    //     field: field,
-    //     from: changedFields[field].previousValue,
-    //     to: changedFields[field].currentValue
-    //   }))
-    // };
-    // this.activityLogs.push(newLogEntry);
+    console.log("Helloyaas");
   }
 }

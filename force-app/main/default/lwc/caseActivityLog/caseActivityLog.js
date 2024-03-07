39ce82e7b5fb311db0c0ddf433f833b230a5b695
danyanuka <danyanuka@gmail.com>
2024-03-07 23:08:57 +0200

    Data displayed and refreshed on change/ Need to clean code

--- force-app/main/default/lwc/caseActivityLog/caseActivityLog.js
+++ force-app/main/default/lwc/caseActivityLog/caseActivityLog.js
@@ -1,7 +1,44 @@
-import { LightningElement, wire, api, track } from "lwc";
-// import { getRecord, recordUpdated } from "lightning/uiRecordApi";
+import { LightningElement, api, track } from "lwc";
 import getCaseActivityLog from "@salesforce/apex/CaseHistoryController.getCaseActivityLog";
-// import CASE_OBJ from "@salesforce/schema/Case"; // Represents the Case object
+import { registerRefreshHandler } from "lightning/refresh";
+
+export default class CaseActivityLog extends LightningElement {
+  @api recordId; // Holds the current record Id (@api for var to be public)
+  // @track caseData;
+  @track activityLog;
+  refreshHandlerId;
+
+  // @wire(getCaseActivityLog, { caseId: "$recordId" })
+  // wiredActivityLog({ data, error }) {
+  //   if (data) {
+  //     this.activityLog = data;
+  //   } else if (error) {
+  //     console.log("Error:", error);
+  //   }
+  // }
+
+  connectedCallback() {
+    this.refreshHandlerId = registerRefreshHandler(this, this.refreshHandler);
+    this.fetchCaseActivities();
+  }
+  refreshHandler() {
+    console.log("Change");
+    return new Promise((resolve) => {
+      this.fetchCaseActivities();
+      resolve(true);
+    });
+  }
+  fetchCaseActivities() {
+    getCaseActivityLog({ caseId: this.recordId })
+      .then((response) => {
+        console.log(response);
+        this.activityLog = response;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
 
 // const FIELDS = [
 //   "Case.CaseNumber",
@@ -12,34 +49,19 @@ import getCaseActivityLog from "@salesforce/apex/CaseHistoryController.getCaseAc
 //   "Case.Priority"
 // ];
 
-export default class CaseActivityLog extends LightningElement {
-  @api recordId; // Holds the current record Id (@api for var to be public)
-  // @track caseData;
-  @track activityLog;
+// get isActivityLogDefined() {
+//   return this.activityLog !== undefined;
+// }
 
-  @wire(getCaseActivityLog, { caseId: "$recordId" })
-  wiredActivityLog({ data, error }) {
-    if (data) {
-      this.activityLog = data;
-      console.log(this.activityLog);
-    } else if (error) {
-      console.log("Error:", error);
-    }
-  }
-
-  // @wire(getRecord, {
-  //   recordId: "$recordId",
-  //   fields: FIELDS
-  // })
-  // caseObjectData({ data, error }) {
-  //   if (data) {
-  //     this.caseData = data.fields;
-  //     console.log("data : ", data.fields);
-  //   } else if (error) {
-  //     console.error("Error:", error);
-  //   }
-  // }
-  connectedCallback() {
-    console.log("Helloyaas");
-  }
-}
+// @wire(getRecord, {
+//   recordId: "$recordId",
+//   fields: FIELDS
+// })
+// caseObjectData({ data, error }) {
+//   if (data) {
+//     this.caseData = data.fields;
+//     console.log("data : ", data.fields);
+//   } else if (error) {
+//     console.error("Error:", error);
+//   }
+// }

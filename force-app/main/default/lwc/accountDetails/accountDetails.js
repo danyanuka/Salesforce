9906a7983c8b7ee7ca066c5667d9b95f4aaa695f
danyanuka <danyanuka@gmail.com>
2024-03-10 19:32:16 +0200

    in Progress/ retrieving the realevant data for accouts Details componenet

--- force-app/main/default/lwc/accountDetails/accountDetails.js
+++ force-app/main/default/lwc/accountDetails/accountDetails.js
@@ -1,17 +1,56 @@
-import { LightningElement, wire, api } from "lwc";
+import { LightningElement, wire, api, track } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
+import ACCOUNT_OBJECT from "@salesforce/schema/Account";
+import TYPE_FIELD from "@salesforce/schema/Account.Type";
+import { getObjectInfo, getPicklistValues } from "lightning/uiObjectInfoApi";
 
 const FIELDS = ["Account.Phone", "Account.Type", "Account.Description"]; //Fiedls to retrieve from Account
 
 export default class AccountRecordComponent extends LightningElement {
   @api recordId; // Current Account record Id passed from Lightning Record Page dynamically
   accountRecord;
+  @track Phone = "";
+  @track Type = "";
+  @track Description = "";
+  accountRecordTypeId;
+  accTypeOptions;
+
   @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
   wiredAccountRecord({ error, data }) {
     if (data) {
-      console.log("data:", data);
+      this.accountRecord = data.fields;
     } else if (error) {
-      console.log(error);
+      console.log("Error :", error);
     }
   }
+
+  //Getting recordTypeId to pass as parameter to the next wire decorator (getPickListValues)
+  @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+  wiredAccTypeOptions({ error, data }) {
+    if (data) {
+      this.accountRecordTypeId = data.defaultRecordTypeId;
+    } else if (error) {
+      console.log("Error :", error);
+    }
+  }
+  //Getting the options of field Type of Account
+  @wire(getPicklistValues, {
+    recordTypeId: "$accountRecordTypeId",
+    fieldApiName: TYPE_FIELD
+  })
+  picklistResults({ error, data }) {
+    if (data) {
+      this.accTypeOptions = data.values.map((option) => option.value);
+      console.log(data.values.map((option) => option));
+    } else if (error) {
+      console.log("Error :", error);
+      this.accTypeOptions = undefined;
+    }
+  }
+
+  handleChange(ev) {
+    console.log("change", ev.target.value);
+  }
+
+  connectedCallback() {}
 }

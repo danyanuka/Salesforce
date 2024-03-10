3cd536297bcce1acd0cf976009544c4f17b3b7d7
danyanuka <danyanuka@gmail.com>
2024-03-10 23:52:04 +0200

    Created apex class for retrieving contact data, created a table to display contact data

--- force-app/main/default/lwc/accountDetails/accountDetails.js
+++ force-app/main/default/lwc/accountDetails/accountDetails.js
@@ -1,21 +1,73 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire } from "lwc";
+import getUnassociatedContact from "@salesforce/apex/ContactController.getUnassociatedContact";
 
-// import ACCOUNT_OBJECT from "@salesforce/schema/Account";
+//Account Related
+import ACCOUNT_OBJECT from "@salesforce/schema/Account";
 import ACCOUNT_PHONE_FIELD from "@salesforce/schema/Account.Phone";
 import ACCOUNT_TYPE_FIELD from "@salesforce/schema/Account.Type";
 import ACCOUNT_DESCRIPTION_FIELD from "@salesforce/schema/Account.Description";
+//Contact Related
+import CONTACT_OBJECT from "@salesforce/schema/Contact";
 
 export default class AccountRecordComponent extends LightningElement {
   @api recordId; // Current Account record Id passed from Lightning Record Page dynamically
-  @api objectApiName;
-  //Fiedls to retrieve from Account
+  // @api objectApiName;
+  unassociatedContacts;
+
+  accountApiName = ACCOUNT_OBJECT;
+  contactApiName = CONTACT_OBJECT;
+  //Expose fields of account
   phoneField = ACCOUNT_PHONE_FIELD;
   typeField = ACCOUNT_TYPE_FIELD;
   descriptionField = ACCOUNT_DESCRIPTION_FIELD;
+
+  @wire(getUnassociatedContact)
+  wiredUnassociatedContacts({ error, data }) {
+    if (data) {
+      console.log(data);
+      this.unassociatedContacts = data;
+    } else if (error) {
+      console.log("Error: ", error);
+    }
+  }
+
+  // @wire(getRecords, {
+  //   objectApiName: CONTACT_OBJECT,
+  //   fields: CONTACT_FIELDS,
+  //   filter: { AccountId: null },
+  //   sortBy: CONTACT_NAME_FIELD,
+  //   pageSize: 10
+  // })
+  // wiredContacts({ error, data }) {
+  //   if (data) {
+  //     console.log(data);
+  //   } else if (error) {
+  //     console.log("Error :", error);
+  //   }
+  // }
+
+  handleContactAssociation() {
+    console.log("hey");
+  }
   connectedCallback() {}
 
-  //import { getRecord } from "lightning/uiRecordApi";
+  //@wire(getRecord, {
+  //   recordId: "$recordId",
+  //   fields: FIELDS
+  // })
+  // caseObjectData({ data, error }) {
+  //   if (data) {
+  //     this.caseData = data.fields;
+  //     console.log("data : ", data.fields);
+  //   } else if (error) {
+  //     console.error("Error:", error);
+  //   }
+  // }
+
   //import TYPE_FIELD from "@salesforce/schema/Account.Type";
+  //import CONTACT_NAME_FIELD from "@salesforce/schema/Contact.Name";
+  // import CONTACT_EMAIL_FIELD from "@salesforce/schema/Contact.Email";
+  // import CONTACT_MOBILEPHONE_FIELD from "@salesforce/schema/Contact.MobilePhone";
 
   // accountRecord;
   // @track Phone = "";
@@ -57,4 +109,10 @@ export default class AccountRecordComponent extends LightningElement {
   //     this.accTypeOptions = undefined;
   //   }
   // }
+
+  // const CONTACT_FIELDS = [
+  //   CONTACT_NAME_FIELD,
+  //   CONTACT_EMAIL_FIELD,
+  //   CONTACT_MOBILEPHONE_FIELD
+  // ];
 }

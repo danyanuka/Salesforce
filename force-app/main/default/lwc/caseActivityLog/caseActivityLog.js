fa9d14d9c6760d09b43a7e4b40d00306b642035e
danyanuka <danyanuka@gmail.com>
2024-03-11 14:30:50 +0200

    Clean comments and logs in caseActivityLog

--- force-app/main/default/lwc/caseActivityLog/caseActivityLog.js
+++ force-app/main/default/lwc/caseActivityLog/caseActivityLog.js
@@ -7,27 +7,20 @@ export default class CaseActivityLog extends LightningElement {
   @track activityLog; //List of Case activities
   refreshHandlerId; // in case there are many handlers
 
-  // @wire(getCaseActivityLog, { caseId: "$recordId" })
-  // wiredActivityLog({ data, error }) {
-  //   if (data) {
-  //     this.activityLog = data;
-  //   } else if (error) {
-  //     console.log("Error:", error);
-  //   }
-  // }
-
   connectedCallback() {
-    this.refreshHandlerId = registerRefreshHandler(this, this.refreshHandler); //Handles record page refresh and refreshes lwc comp
+    this.refreshHandlerId = registerRefreshHandler(this, this.refreshHandler); //Identifies record page refresh and triggers LWC componenet refresh, takes context and a funtion as params
     this.fetchCaseActivities();
   }
+
   //Must return a promise
   refreshHandler() {
     console.log("Change");
     return new Promise((resolve) => {
       this.fetchCaseActivities();
       resolve(true);
-    }); // has to return a promise
+    });
   }
+
   async fetchCaseActivities() {
     try {
       const response = await getCaseActivityLog({ caseId: this.recordId });
@@ -39,29 +32,3 @@ export default class CaseActivityLog extends LightningElement {
     }
   }
 }
-
-// const FIELDS = [
-//   "Case.CaseNumber",
-//   "Case.SuppliedEmail",
-//   "Case.ContactId",
-//   "Case.Status",
-//   "Case.OwnerId",
-//   "Case.Priority"
-// ];
-
-// get isActivityLogDefined() {
-//   return this.activityLog !== undefined;
-// }
-
-// @wire(getRecord, {
-//   recordId: "$recordId",
-//   fields: FIELDS
-// })
-// caseObjectData({ data, error }) {
-//   if (data) {
-//     this.caseData = data.fields;
-//     console.log("data : ", data.fields);
-//   } else if (error) {
-//     console.error("Error:", error);
-//   }
-// }
